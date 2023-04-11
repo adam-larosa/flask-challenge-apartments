a7d045d4282acccd47f3d844366eafe5dcb36ab8
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-11 13:41:25 -0700

    fin :)

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from sqlalchemy.exc import IntegrityError
 
-from models import db
+from models import db, Tenant
+
+import ipdb
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
@@ -10,6 +13,65 @@ app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
 
 migrate = Migrate( app, db )
 db.init_app( app )
+api = Api( app )
+
+
+
+class Tenants( Resource ):
+    def get( self ):
+        tenants_list = []
+        for t in Tenant.query.all():
+            t_dict = {
+                'id': t.id,
+                'name': t.name,
+                'age': t.age
+            }
+            tenants_list.append( t_dict )
+        return make_response( tenants_list, 200 )
+
+    def post( self ):
+        data = request.get_json()
+        new_tenant = Tenant( name = data['name'], age = data['age'] )
+        try:
+            db.session.add( new_tenant )
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return make_response( {'error': 'validation errors'}, 422 )
+        return make_response( new_tenant.to_dict(), 201 )
+
+api.add_resource( Tenants, '/tenants' )
+
+
+class TenantsById( Resource ):
+
+    def get( self, id ):
+        t = Tenant.query.filter_by( id = id ).first()
+        if t == None:
+            return make_response( { 'msg': 'Tenant not found' }, 404 )
+        return make_response( t.to_dict(), 200 )
+
+    def patch( self, id ):
+        t = Tenant.query.filter_by( id = id ).first()
+        if t == None:
+            return make_response( { 'msg': 'Tenant not found' }, 404 )
+        data = request.get_json()
+        for key in data.keys():
+            setattr( t, key, data[key] )
+        db.session.add( t )
+        db.session.commit()
+        return make_response( t.to_dict(), 200 )
+
+    def delete( self, id ):
+        t = Tenant.query.filter_by( id = id ).first()
+        if t == None:
+            return make_response( { 'msg': 'Tenant not found' }, 404 )
+        db.session.delete( t )
+        db.session.commit()
+        return make_response( {'msg': 'success'}, 200 )
+
+api.add_resource( TenantsById, '/tenants/<int:id>')
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -5,3 +5,41 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
+
+class Lease( db.Model, SerializerMixin ):
+    __tablename__ = 'leases'
+    id = db.Column( db.Integer, primary_key = True )
+    rent = db.Column( db.Float )
+    apartment_id = db.Column( db.Integer, db.ForeignKey( 'apartments.id' ) )
+    tenant_id = db.Column( db.Integer, db.ForeignKey( 'tenants.id' ) )
+
+class Apartment( db.Model, SerializerMixin ):
+    __tablename__ = 'apartments' 
+    id = db.Column( db.Integer, primary_key = True )
+    number = db.Column( db.String )
+
+    leases = db.relationship( 'Lease', backref = 'apartment' )
+    tenants = association_proxy( 'leases', 'tenant' )
+
+
+
+class Tenant( db.Model, SerializerMixin ):
+    __tablename__ = 'tenants'
+
+    serialize_rules = ( '-leases.tenant', '-leases.apartment' )
+
+    id = db.Column( db.Integer, primary_key = True )
+    name = db.Column( db.String, nullable = False )
+    age = db.Column( db.Integer, db.CheckConstraint( 'age >= 18' ), 
+                     nullable = False  )
+    leases = db.relationship( 'Lease', backref = 'tenant' )
+
+    apartments = association_proxy( 'leases', 'apartment' )
+    # @property      # <----- another way to get the apartment out of the lease
+    # def apartments( self ):
+    #     return [ l.apartment for l in self.leases ]
+   
+
+    __table_args__ = (
+        db.CheckConstraint( 'age >= 18' ),
+    )
